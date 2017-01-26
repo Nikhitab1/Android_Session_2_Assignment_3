2b14a3fb19f8586ad1cd98fbc356b1b591ba77d2
Nikhitab1 <b.nikhitha.sai@gmail.com>
2017-01-26 17:08:13 +0530

    Commit
    Commit

---
+++ app/src/main/java/com/example/android/loginscreen/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.loginscreen;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar bar = getActionBar();
+//for color
+
+    }
+}
